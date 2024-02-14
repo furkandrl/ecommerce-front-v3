62920a3b824d7f7d93d8e500a91729e0f56ff293
Furkan <frkndereli@gmail.com>
2024-02-14 17:13:32 +0300

    app component developments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { AccountService } from './services/account.service';
+import { ProductService } from './services/product.service';
+import { CustomerLogin } from './interfaces/requests/customer-login';
+import { CustomerLoginRes } from './interfaces/responses/customer-login-res';
+import { Router } from '@angular/router';
+import { CustomerRegister } from './interfaces/requests/customer-register';
+import { CustomerRes } from './interfaces/responses/customer-res';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ecommerce-front-v3';
+
+  loggedCustomer:any = {};
+/*
+  loginObj:CustomerLogin={
+    username:'',
+    password:''
+  };*/
+
+  constructor(private accountService:AccountService, private productService:ProductService, 
+    router:Router){}
+
+  onRegister(registerObj:CustomerRegister){
+    this.accountService.registerCustomer(registerObj).subscribe((res:CustomerRes) => {
+
+    },
+    (error:Error) => alert(error.message))
+    
+  }
+
+  onLogin(loginObj:CustomerLogin){
+    this.accountService.loginCustomer(loginObj).subscribe((res:CustomerLoginRes) => {
+      if(res.jwtToken){
+        localStorage.removeItem('customer_token')
+        localStorage.setItem('customer_token', res.jwtToken)
+        this.loggedCustomer=localStorage.getItem('customer_token')
+        this.router.naviagteByUrl('/')
+      }
+    },
+    (error:Error) => alert(error.message))
+  }
+
+
+
 }
