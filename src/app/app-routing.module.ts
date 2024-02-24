e81be1f828a4bf1e1ba29f1aa5572bf0acf67aa6
Furkan Dereli <frkndereli@gmail.com>
2024-02-24 22:47:29 +0300

    product detail page improvement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CartComponent } from './components/cart/cart.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 const routes: Routes = [
   {
@@ -12,8 +13,11 @@ const routes: Routes = [
   {
     path:'cart',
     component: CartComponent
+  },
+  {
+    path:'product',
+    component: ProductDetailComponent
   }
-  
 ];
 
 @NgModule({
